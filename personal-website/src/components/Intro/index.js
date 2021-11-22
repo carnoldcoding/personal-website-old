cc43997d5ea4772eb1a9616452322e8f2fdf97e1
carnoldcoding <carnold1@ithaca.edu>
2021-11-21 19:26:10 -0500

    first-commit

---
+++ personal-website/src/components/Intro/index.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+import Video from '../../videos/video.mp4'
+import { IntroContainer, VideoBg, IntroBg, IntroContent, IntroH1, IntroP, IntroBtnWrapper, ArrowForward, ArrowRight } from './IntroElements'
+import {Button } from '../ButtonElement'
+
+const Intro = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+    return (
+        <IntroContainer>
+            <IntroBg>
+                <VideoBg autoPlay loop muted src={Video} type = 'video/mp4' />
+            </IntroBg>
+            <IntroContent>
+                <IntroH1>Carnold Coding</IntroH1>
+                <IntroP>
+                    This is my website fuckers, enjoy it or explode
+                </IntroP>
+                <IntroBtnWrapper>
+                    <Button to="about" onMouseEnter={onHover} onMouseLeave={onHover} primary = 'true' dark = 'true'>
+                        Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </IntroBtnWrapper>
+            </IntroContent>
+        </IntroContainer>
+    )
+}
+
+export default Intro
---
+++ personal-website/src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, SidebarRoute } from './SidebarElements'
+
+const Sidebar = ({isOpen, toggle}) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon />
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    
+                    <SidebarLink to="about" onClick={toggle}>
+                        About
+                    </SidebarLink>
+
+                    <SidebarLink to="projects" onClick={toggle}>
+                        Projects
+                    </SidebarLink>
+
+                    <SideBtnWrap>
+                        <SidebarRoute to ="/contact">Contact</SidebarRoute>
+                    </SideBtnWrap>
+                
+                </SidebarMenu>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
