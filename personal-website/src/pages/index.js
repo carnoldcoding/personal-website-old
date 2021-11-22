f0ff1adc4888f6a6a829e9f6abbf57233626bb87
carnoldcoding <carnold1@ithaca.edu>
2021-11-21 20:39:04 -0500

    Added the ground work for the second section.

--- personal-website/src/pages/index.js
+++ personal-website/src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar' 
 import IntroSection from '../components/Intro'
+import InfoSection from '../components/Info'
 
 const Home = () => {
 
@@ -15,6 +16,7 @@ const Home = () => {
         <Sidebar isOpen={isOpen} toggle={toggle}/>
         <Navbar toggle={toggle}/>
         <IntroSection />
+        <InfoSection />
         </>
     )
 }
