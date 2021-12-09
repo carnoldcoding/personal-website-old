09870cc8447f495c946b926f8325f95312d1b74f
carnoldcoding <carnold1@ithaca.edu>
2021-12-09 15:10:20 -0500

    Everything is in line, only glitch is the margin on the right side in mobile.

--- personal-website/src/components/Services/ServicesElements.js
+++ personal-website/src/components/Services/ServicesElements.js
@@ -42,7 +42,8 @@ export const ServicesCard = styled.div`
     justify-content: flex-start;
     align-items: center;
     border-radius: 10px;
-    max-height: 340px;
+    max-height: 380px;
+    min-height: 340px;
     padding: 30px;
     box-shadow: 0 1px 3px rgba(0,0,0,0.2);
     transition: all .2s ease-in-out;
